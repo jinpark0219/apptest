02503aea078093b337c3dfd18d26b3c3a0fccd4b
Jason Park <jkpark0314@gmail.com>
2021-12-30 19:57:45 +0900

    first commit

---
+++ router/main.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser');
+
+module.exports = function(app) {
+     app.use(bodyParser.json());       // to support JSON-encoded bodies
+     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+         extended: true
+      }));
+     
+     app.get('/',function(req,res){
+        res.render('index.html')
+     });
+
+     app.get('/about',function(req,res){
+        res.render('about.html');
+    });
+
+    app.get('/blog',function(req,res){
+      res.render('blog.html');
+  });
+
+  app.get('/client',function(req,res){
+   res.render('client.html');
+});
+
+app.get('/contact',function(req,res){
+   res.render('contact.html');
+});
+     
+    app.get('/listall', function (req, res) {
+      //res.render('about.html');
+      console.log('listall...');
+      const getNumber = req.body.getNumber;
+      let response = {
+            'result': 'true',
+            'getLists': getNumber 
+      }
+      
+      console.log('response : ' + response);
+      res.status(200).json(response);
+   });  // end of get
+     
+}
